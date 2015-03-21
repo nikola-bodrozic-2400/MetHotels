8383be5e53954d2d7ec6eb172111d4d80a3bc0f1
Nikola Bodrozic 2400 <nikola.bodrozic.2400@example.com>
2015-03-21 09:06:19 -0700

    Dodao checkbox-ove za TV, internet i dzakuzi

--- src/main/java/com/mycompany/methotels/data/Soba.java
+++ src/main/java/com/mycompany/methotels/data/Soba.java
@@ -17,6 +17,9 @@ public class Soba {
 
     private String imeSobe;
     private int sprat;
+    private boolean tv;
+    private boolean internet;
+    private boolean djakuzi;
 
     @Inject
     public Soba() {
@@ -27,20 +30,49 @@ public class Soba {
         this.sprat = sprat;
     }
 
+    // soba
     public String getImeSobe() {
         return imeSobe;
     }
-
-    public int getSprat() {
-        return sprat;
-    }
-
+    
     public void setImeSobe(String imeSobe) {
         this.imeSobe = imeSobe;
     }
 
+    // sprat
+    public int getSprat() {
+        return sprat;
+    }
+
     public void setSprat(int sprat) {
         this.sprat = sprat;
+    }
+
+    // TV
+    public boolean isTv() {
+        return tv;
+    }
+    
+    public void setTv(boolean tv) {
+        this.tv = tv;
+    }
+    
+    // Internet
+    public boolean isInternet() {
+        return internet;
+    }
+    
+    public void setInternet(boolean internet) {
+        this.internet = internet;
+    }    
+
+    // Djakuzi
+    public boolean isDjakuzi() {
+        return djakuzi;
+    }
+    
+    public void setDjakuzi(boolean djakuzi) {
+        this.djakuzi = djakuzi;
     }
     
 }
